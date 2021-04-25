dec92cf6942540688da560a7ab2ac8ed8a943d53
James Wright <james@jamesswright.co.uk>
2021-04-25 16:17:05 +0100

    Introduces lambda for handling recent events

---
+++ handlers/list-recent-events/index.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const { MongoClient } = require('mongodb');
+const { MONGODB_URI } = process.env;
+
+let conn;
+
+const getDBConnection = async () => {
+  if (!conn) {
+    conn = await MongoClient.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  }
+
+  return conn;
+};
+
+const createRes = (statusCode, body) => ({
+  statusCode,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  isBase64Encoded: false,
+  body: JSON.stringify(body),
+});
+
+exports.handler = async () => {
+  const connection = await getDBConnection();
+
+  try {
+    const events = await connection
+      .db('events')
+      .collection('events')
+      .find({}, { // TODO: create index
+        limit: 10,
+        sort: [['date', -1]],
+      })
+      .toArray();
+
+    return createRes(200, events);
+  } catch ({ message }) {
+    return createRes(500, {
+      message,
+    });
+  }
+};
