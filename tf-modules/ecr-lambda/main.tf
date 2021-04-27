8ed64991c8bec1f7e5557e1986a3fba5268697d7
James Wright <james@jamesswright.co.uk>
2021-04-27 18:27:18 +0100

    Removes redundant args from lambda function resource

--- tf-modules/ecr-lambda/main.tf
+++ tf-modules/ecr-lambda/main.tf
@@ -33,7 +33,5 @@ resource "aws_lambda_function" "lambda_function" {
   function_name = var.function_name
   package_type  = "Image"
   image_uri     = "${aws_ecr_repository.lambda_repo.repository_url}:latest"
-  handler       = "index.handler"
-  runtime       = "nodejs14.x"
   role          = aws_iam_role.lambda_role.arn
 }
