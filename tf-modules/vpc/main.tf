e4804ac094b3522968bc2363413e880f125e9495
James Wright <james@jamesswright.co.uk>
2021-04-28 12:02:57 +0100

    Adds ingress to security group and TODO

--- tf-modules/vpc/main.tf
+++ tf-modules/vpc/main.tf
@@ -36,16 +36,20 @@ resource "aws_subnet" "subnet_c" {
   }
 }
 
+# TODO: split into respective security groups for
+# lambdas (egress) and VPC endpoint (ingress)
 resource "aws_security_group" "security_group" {
-  # Given that we're communicating between lambda
-  # and our Atlas endpoint via our VPC endpoint,
-  # we have no rules to configure right now.
-  # However, a security group is nonetheless
-  # required to create said VPC endpoint.
-
   vpc_id = aws_vpc.iot_events.id
   name = "iot-events-default-security-group"
   description = "The default security group for the IoT events VPC"
+
+  ingress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 
   egress {
     from_port        = 0
