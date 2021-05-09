cfdc13e759c2ce60c0944746a92f66dcc7570aa4
James Wright <james@jamesswright.co.uk>
2021-05-09 15:28:14 +0100

    Formats integration JS module

--- local-api-gateway/integration.js
+++ local-api-gateway/integration.js
@@ -6,10 +6,13 @@ export default function invoke(r) {
     queryParameters: r.args,
   };
 
-  r.subrequest(`http://${r.variables.handler_host}/2015-03-31/functions/function/invocations`, {
-    method: 'POST',
-    body: JSON.stringify(payload),
-  }).then(function (res) {
+  r.subrequest(
+    `http://${r.variables.handler_host}/2015-03-31/functions/function/invocations`,
+    {
+      method: 'POST',
+      body: JSON.stringify(payload),
+    },
+  ).then(function (res) {
     r.return(res.status, res.responseBody);
   });
 }
